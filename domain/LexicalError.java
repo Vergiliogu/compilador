75b332ca479cb5dbcac916a4950e219c993c0c78
Vergiliogu <vergiliopoleza@gmail.com>
2024-09-05 20:33:51 -0300

    feat: add domain folder

---
+++ domain/LexicalError.java
@@ -0,0 +1,12 @@
+public class LexicalError extends AnalysisError
+{
+    public LexicalError(String msg, int position)
+	 {
+        super(msg, position);
+    }
+
+    public LexicalError(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ domain/SemanticError.java
@@ -0,0 +1,12 @@
+public class SemanticError extends AnalysisError
+{
+    public SemanticError(String msg, int position)
+	 {
+        super(msg, position);
+    }
+
+    public SemanticError(String msg)
+    {
+        super(msg);
+    }
+}
