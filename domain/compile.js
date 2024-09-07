c0ecb9557e0654bad49704d7e84e5e7add6c710f
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2024-09-07 16:44:22 -0300

    chore(domain): added node example and `README` file

---
+++ domain/compile.js
@@ -0,0 +1,18 @@
+import { exec } from 'child_process';
+
+const javaInstance = 'C:/Users/lemue/.jdks/corretto-17.0.12/bin/java.exe -cp';
+
+const classPath = './bin/classes'
+
+const className = 'com.domain.compiler.App'
+
+const sourceCodeFile = './resources/source-code.txt'
+
+exec(`${javaInstance} ${classPath} ${className} ${sourceCodeFile}`, (err, stdout, _stderr) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(stdout);
+    console.log(_stderr);
+});
