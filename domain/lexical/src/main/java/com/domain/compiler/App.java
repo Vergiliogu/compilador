6a1bb7b505caac80d6c9114ae2a7bf18b2daca31
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2024-09-07 15:43:45 -0300

    feat(domain): added jackson dependency to parse json

--- domain/lexical/src/main/java/com/domain/compiler/App.java
+++ domain/lexical/src/main/java/com/domain/compiler/App.java
@@ -1,14 +1,36 @@
 package com.domain.compiler;
 
 import com.domain.lexical.LexicalAnalyser;
+import com.domain.lexical.LexicalResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-        String sourceCode = "teste";
+        String filePath = args[0];
 
-        LexicalAnalyser lexicalAnalyser = new LexicalAnalyser(sourceCode);
+        File sourceCodeFile = new File(filePath);
 
-        lexicalAnalyser.run();
+        try {
+            List<String> lines = Files.readAllLines(sourceCodeFile.toPath());
+
+            LexicalAnalyser lexicalAnalyser = new LexicalAnalyser(String.join("\n", lines));
+
+            LexicalResponse response = lexicalAnalyser.run();
+
+            String responseAsJson = new ObjectMapper()
+                    .writer()
+                    .withDefaultPrettyPrinter()
+                    .writeValueAsString(response);
+
+            System.out.println(responseAsJson);
+        } catch (IOException e) {
+            System.err.println("File not found for path: " + filePath);
+        }
     }
 }
