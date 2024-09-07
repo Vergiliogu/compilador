d639975e5f22a1b035229bb2b7456ce4f7ad1e96
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2024-09-07 16:51:33 -0300

    chore(domain): added node integration example

--- domain/compile.js
+++ domain/compile.js
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 
-const javaInstance = 'C:/Users/lemue/.jdks/corretto-17.0.12/bin/java.exe';
+const javaPath = 'C:/Users/lemue/.jdks/corretto-17.0.12/bin/java.exe';
 
 const classPath = '-cp ./bin/classes'
 
@@ -8,7 +8,7 @@ const className = 'com.domain.compiler.App'
 
 const sourceCodeFile = './resources/source-code.txt'
 
-exec(`${javaInstance} ${classPath} ${className} ${sourceCodeFile}`, (err, stdout, _stderr) => {
+exec(`${javaPath} ${classPath} ${className} ${sourceCodeFile}`, (err, stdout, _stderr) => {
     if (err) {
         console.error(err);
         return;
