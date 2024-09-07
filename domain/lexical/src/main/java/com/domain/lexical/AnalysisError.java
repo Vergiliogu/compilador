9680faee95fd74f43bdb4d8cbfe8cc9d2bc5edeb
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2024-09-07 09:11:32 -0300

    style(domain): fixed lexical style; removed not used records

--- domain/lexical/src/main/java/com/domain/lexical/AnalysisError.java
+++ domain/lexical/src/main/java/com/domain/lexical/AnalysisError.java
@@ -5,32 +5,27 @@ public class AnalysisError extends Exception {
     private final int position;
     private final String lexeme;
 
-    public AnalysisError(String msg, int position, String lexeme)
-    {
+    public AnalysisError(String msg, int position, String lexeme) {
         super(msg);
         this.position = position;
         this.lexeme = lexeme;
     }
 
-    public AnalysisError(String msg, String lexeme)
-    {
+    public AnalysisError(String msg, String lexeme) {
         super(msg);
         this.position = -1;
         this.lexeme = lexeme;
     }
 
-    public int getPosition()
-    {
+    public int getPosition() {
         return position;
     }
 
-    public String getLexeme()
-    {
+    public String getLexeme() {
         return lexeme;
     }
 
-    public String toString()
-    {
+    public String toString() {
         return super.toString() + ", @ "+position;
     }
 }
