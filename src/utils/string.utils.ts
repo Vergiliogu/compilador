50b15b42705ee295caff764c61367b82d5c8fed9
Vergiliogu <vergiliopoleza@gmail.com>
2024-09-15 17:34:28 -0300

    fix: lexical table with wrong header

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -5,7 +5,7 @@ export const addSpacesAtTheEnd = (text: string, targetLength: number): string =>
 }
 
 export const createLexicalTable = (tokens: Token[]): string => {
-  let table = addSpacesAtTheEnd('linha', 10) + addSpacesAtTheEnd('classe', 20) + 'Line\n';
+  let table = addSpacesAtTheEnd('linha', 10) + addSpacesAtTheEnd('classe', 20) + 'lexema\n';
 
   tokens.forEach(token => {
     table += `${addSpacesAtTheEnd(token.lineNumber, 10)}${addSpacesAtTheEnd(token.id, 20)}${token.lexeme}\n`;
