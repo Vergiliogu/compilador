309e5e0200262351e067fb67785f484bd99b357a
Lemuel Kauê Manske de Liz <lemuelkauemanskede.liz@philips.com>
2024-11-14 20:18:46 -0300

    feat: ilasm writer api

--- domain/compiler/src/main/java/compiler/App.java
+++ domain/compiler/src/main/java/compiler/App.java
@@ -1,7 +1,11 @@
 package compiler;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
 
@@ -10,25 +14,47 @@ public class App {
     private static final String FORMATTED_ERROR_MESSAGE = "Erro na linha %d - %s";
     private static final String PROGRAM_COMPILED_MESSAGE = "programa compilado com sucesso";
     private static final String FILE_COULD_NOT_BE_LOADED_MESSAGE = "arquivo não pode ser carregado: %s";
+    private static final String FILE_COULD_NOT_BE_CREATED_MESSAGE = "arquivo ilasm %s não pode ser escrito";
 
     public static void main(String[] args) {
         String filePath = args[0];
         File sourceCodeFile = new File(filePath);
 
-        String output = PROGRAM_COMPILED_MESSAGE;
+        Semantic semantic = new Semantic();
 
         try {
             List<String> lines = Files.readAllLines(sourceCodeFile.toPath());
 
-            Semantic semantic = new Semantic();
             Lexical lexical = new Lexical(String.join("\n", lines));
             Syntactic syntactic = new Syntactic(lexical, semantic);
 
             syntactic.run();
         }
-        catch (IOException e) { output = FILE_COULD_NOT_BE_LOADED_MESSAGE.formatted(sourceCodeFile.toPath()); }
-        catch (CompilationError e) { output = FORMATTED_ERROR_MESSAGE.formatted(e.getLineNumber(), e.getErrorMessage()); }
 
-        System.out.printf(output);
+        catch (IOException e) {
+            System.out.printf(FILE_COULD_NOT_BE_LOADED_MESSAGE.formatted(sourceCodeFile.toPath()));
+            return;
+        }
+
+        catch (CompilationError e) {
+            System.out.printf(FORMATTED_ERROR_MESSAGE.formatted(e.getLineNumber(), e.getErrorMessage()));
+            return;
+        }
+
+        writeIlasmFile(sourceCodeFile, semantic.getObjectCode());
+
+        System.out.printf(PROGRAM_COMPILED_MESSAGE);
+    }
+
+    private static void writeIlasmFile(final File sourceCodeFile, final String objectCodeContent) {
+        String fileName = sourceCodeFile.getAbsolutePath();
+
+        File ilasmFile = new File("%s.il".formatted(fileName));
+
+        try (FileOutputStream fileInputStream = new FileOutputStream(ilasmFile)) {
+            fileInputStream.write(objectCodeContent.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            System.out.println(FILE_COULD_NOT_BE_CREATED_MESSAGE.formatted(fileName));
+        }
     }
 }
--- domain/compiler/src/main/java/compiler/Semantic.java
+++ domain/compiler/src/main/java/compiler/Semantic.java
@@ -1,11 +1,37 @@
 package compiler;
 
+import java.io.FileOutputStream;
+
 /**
  * Runs the semantic analysis.
  */
 public class Semantic {
 
+    private static final String HEADER =
+    """
+    .assembly extern mscorlib {}
+    .assembly _codigo_objeto{}
+    .module _codigo_objeto.exe
+    .class public UNICA{
+    .method static public void _principal() {
+    .entrypoint
+    """;
+
+    private static final String FOOTER =
+    """
+    ret
+    }
+    }
+    """;
+
     public void executeAction(int action, Token token)	throws CompilationError {
-        throw new CompilationError("Not implemented yet.", -1);
+
+    }
+
+    /**
+     * Once the semantic analysis ran, the object code will output from here.
+     */
+    public String getObjectCode() {
+        return HEADER + "\n" + FOOTER;
     }
 }
